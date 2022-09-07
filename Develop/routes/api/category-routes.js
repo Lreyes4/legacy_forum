cfb496e217b959e2304aeffd5f0970f4a6d19a47
Lreyes4 <103380089+Lreyes4@users.noreply.github.com>
2022-09-06 21:24:56 -0600

    added some of the routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,18 +3,40 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/categories', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({include: [Product]})
+  .then(categories => {
+    res.json(categories)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
 });
 
-router.get('/:id', (req, res) => {
+router.get('/categories/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({where: {id: req.params.id}, include: [Product]})
+  .then(category => {
+    res.json(category)
+  })
+  .catch(err => {
+    console.log(err)
+  })
 });
 
-router.post('/', (req, res) => {
+router.post('/categories', (req, res) => {
   // create a new category
+  Category.create(req.body)
+  .then(category => {
+    res.json(category)
+  })
+  .catch(err => {
+    console.log(err)
+  })
 });
 
 router.put('/:id', (req, res) => {
