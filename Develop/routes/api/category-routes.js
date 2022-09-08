e5d3d35eb833b65ebba69f8747119186be234126
Lreyes4 <103380089+Lreyes4@users.noreply.github.com>
2022-09-07 18:45:50 -0600

    added category and product routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,12 +3,12 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/categories', (req, res) => {
+router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
   Category.findAll({include: [Product]})
-  .then(categories => {
-    res.json(categories)
+  .then(data => {
+    res.json(data)
   })
   .catch(err => {
     console.log(err)
@@ -16,23 +16,23 @@ router.get('/categories', (req, res) => {
 
 });
 
-router.get('/categories/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   Category.findOne({where: {id: req.params.id}, include: [Product]})
-  .then(category => {
-    res.json(category)
+  .then(data => {
+    res.json(data)
   })
   .catch(err => {
     console.log(err)
   })
 });
 
-router.post('/categories', (req, res) => {
+router.post('/', (req, res) => {
   // create a new category
   Category.create(req.body)
-  .then(category => {
-    res.json(category)
+  .then(data => {
+    res.json(data)
   })
   .catch(err => {
     console.log(err)
@@ -41,10 +41,28 @@ router.post('/categories', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {id: req.params.id}
+  })
+  .then(data => {
+    res.json(data)
+  })
+  .catch(err => {
+    console.log(err)
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {id: req.params.id}
+  })
+  .then(data => {
+    res.json(data)
+  })
+  .catch(err => {
+    console.log(err)
+  })
 });
 
 module.exports = router;
